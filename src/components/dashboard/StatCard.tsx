537dbf19053beba6ef7eed4aa0ad9fa1e4eecbe5
Euclides Medina <euclidesmeda10@gmail.com>
2025-05-01 15:44:01 -0500

    actualizando StatCard para dar color al valor

--- src/components/dashboard/StatCard.tsx
+++ src/components/dashboard/StatCard.tsx
@@ -3,17 +3,18 @@ import { Card } from '../ui/Card';
 interface StatCardProps {
   title: string;
   value: string;
+  valueClassName?: string;
   change: string;
   icon?: React.ReactNode;
 }
 
-export const StatCard: React.FC<StatCardProps> = ({ title, value, change, icon }) => {
+export const StatCard: React.FC<StatCardProps> = ({ title, value, valueClassName, change, icon }) => {
   return (
     <Card className="h-full">
       <div className="flex justify-between items-start">
         <div>
           <h3 className="text-gray-600 text-sm mb-1">{title}</h3>
-          <p className="text-2xl font-bold">{value}</p>
+          <p className={`text-2xl font-bold ${valueClassName}`} >{value}</p>
           <p className="text-sm text-gray-500 mt-1">{change}</p>
         </div>
         {icon && <div className="text-gray-400">{icon}</div>}
