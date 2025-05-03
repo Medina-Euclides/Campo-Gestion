864be0f2dc195359f7223e6e72f12ac9bfc77154
Euclides Medina <euclidesmeda10@gmail.com>
2025-05-03 16:04:07 -0500

    solucion de fondo negro modal nueva transaccion

--- src/components/Modals/AddTransation.tsx
+++ src/components/Modals/AddTransation.tsx
@@ -75,7 +75,7 @@ export const NewTransactionModal: React.FC<NewTransactionModalProps> = ({
   };
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
+    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4">
       <div className="bg-white rounded-lg w-full max-w-md my-8">
         <div className="max-h-[90vh] overflow-y-auto">
           <div className="p-6">
