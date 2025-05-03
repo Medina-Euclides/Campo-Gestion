4b79a4890323793d97df5d60e981414c2528948a
Euclides Medina <euclidesmeda10@gmail.com>
2025-05-03 14:55:07 -0500

    solucion fondo negro sidebar menu

--- src/components/layaout/Sidebar-menu.tsx
+++ src/components/layaout/Sidebar-menu.tsx
@@ -51,7 +51,7 @@ export const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
       <>
         {isOpen && (
           <div 
-            className="fixed inset-0 bg-black bg-opacity-50 z-30"
+            className="fixed inset-0 bg-black/50 z-30"
             onClick={onClose}
           />
         )}
