16a8e59600c5acd381c1c4d0f691599c31f22b28
Euclides Medina <euclidesmeda10@gmail.com>
2025-05-16 18:51:09 -0500

    añadiendo credenciales de firebase

---
+++ src/credenciales.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyARwUQGfrmkuX4ehegMJob-5-IJleVqvJY",
+  authDomain: "prueba-campo-d8d9a.firebaseapp.com",
+  projectId: "prueba-campo-d8d9a",
+  storageBucket: "prueba-campo-d8d9a.firebasestorage.app",
+  messagingSenderId: "788838124129",
+  appId: "1:788838124129:web:57508a0a8195a84c013820"
+};
+
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig);
+export default appFirebase;
