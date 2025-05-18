587fe3be2d79979446d06ff8b04dc88d1f224899
Euclides Medina <euclidesmeda10@gmail.com>
2025-05-18 12:48:52 -0500

    añadiendo logica para regirtar usuario en firebase

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,12 +1,15 @@
 //contexto global para saber si un usuario esta logueado
 
-import { createContext, ReactNode, useContext, useEffect, useState} from "react";
+import { createContext, ReactNode, use, useContext, useEffect, useState} from "react";
+import { createUserWithEmailAndPassword, getIdToken } from "firebase/auth";
+import { auth } from "../firebase";
 
 type AuthContextType = {
     isAuthenticated: boolean;
     token: string | null;
     login: (token:string) => void;
     logout: () => void;
+    singUp: (email:string, password: string) => Promise<void>;
 };
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -33,12 +36,18 @@ export const AuthProvider = ({children}:{children: ReactNode}) => {
         localStorage.removeItem('token');
         setToken(null)
     }
+
+    const singUp = async (email:string, password:string) =>{
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+        const idToken = await getIdToken(userCredentials.user);
+        login(idToken);
+    }
     if (isLoading) {
         return <div>Loading...</div>; // O un componente de carga
     }
 
   return (
-    <AuthContext.Provider value={{isAuthenticated: !!token, token, login, logout}}>
+    <AuthContext.Provider value={{isAuthenticated: !!token, token, login, logout, singUp}}>
         {children}
     </AuthContext.Provider>
   )
