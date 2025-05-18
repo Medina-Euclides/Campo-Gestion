485e127659b6f6f20abf0cd70e636a66c0e08593
Euclides Medina <euclidesmeda10@gmail.com>
2025-05-18 16:09:02 -0500

    creando funciones para regitrar y auntenticar en firebase

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,15 +1,16 @@
 //contexto global para saber si un usuario esta logueado
 
-import { createContext, ReactNode, use, useContext, useEffect, useState} from "react";
-import { createUserWithEmailAndPassword, getIdToken } from "firebase/auth";
+import { createContext, ReactNode, useContext, useEffect, useState} from "react";
+import { createUserWithEmailAndPassword, getIdToken, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 
 type AuthContextType = {
     isAuthenticated: boolean;
     token: string | null;
-    login: (token:string) => void;
+    persistSession: (token:string) => void;
     logout: () => void;
-    singUp: (email:string, password: string) => Promise<void>;
+    signUp: (email:string, password: string) => Promise<void>;
+    loginWithEmail: (email:string, password: string) => Promise<void>;
 };
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -27,7 +28,7 @@ export const AuthProvider = ({children}:{children: ReactNode}) => {
         setIsLoading(false)
     },[])
 
-    const login = (newToken: string) =>{
+    const persistSession = (newToken: string) =>{
         localStorage.setItem('token', newToken);
         setToken(newToken)
     }
@@ -37,17 +38,23 @@ export const AuthProvider = ({children}:{children: ReactNode}) => {
         setToken(null)
     }
 
-    const singUp = async (email:string, password:string) =>{
+    const loginWithEmail = async (email:string, password:string) =>{
+        const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+        const idToken = await userCredentials.user.getIdToken();
+        persistSession(idToken);
+    }
+
+    const signUp = async (email:string, password:string) =>{
         const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
         const idToken = await getIdToken(userCredentials.user);
-        login(idToken);
+        persistSession(idToken);
     }
     if (isLoading) {
         return <div>Loading...</div>; // O un componente de carga
     }
 
   return (
-    <AuthContext.Provider value={{isAuthenticated: !!token, token, login, logout, singUp}}>
+    <AuthContext.Provider value={{isAuthenticated: !!token, token, persistSession, logout, signUp, loginWithEmail}}>
         {children}
     </AuthContext.Provider>
   )
