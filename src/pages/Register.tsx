f5be4668e99a980c20f8833ae624192fafc72495
Euclides Medina <euclidesmeda10@gmail.com>
2025-05-18 11:58:16 -0500

    reparando errores: register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,28 +1,44 @@
 import { Button } from "../components/ui/Button"
 import { Input } from "../components/ui/Input"
-
+import { useState } from "react"
 
 const Register = () => {
+    const [user, setUser] = useState({
+            email: '',
+            password: ''})
+
+    const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setUser({...user,[name]:value});
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(user);
+    }
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
         <div className="max-w-md w-full space-y-8 bg-white p-10 rounded-xl shadow-lg">
             <h2 className="text-2xl font-bold text-gray-900">Registrarse a CampoGestión</h2>
         
-            <form className="mt-8 space-y-6">
+            <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
                 <div className="rounded-md shadow-sm -space-y-px">
                     <Input
-                    id="nombre"
-                    name="nombre"
-                    label="Correo"
-                    placeholder="Nombre"
-                    type="email"
+                        id="email"
+                        name="email"
+                        label="Usuario"
+                        placeholder="usuario@gmail.com"
+                        type="email"
+                        onChange={handleChange}
                     />
+
                     <Input
-                    id="nombre"
-                    name="nombre"
-                    label="Contraseña"
-                    placeholder="Nombre"
-                    type="email"
+                        id="password"
+                        name="password"
+                        label="Contraseña"
+                        placeholder="********"
+                        type="password"
+                        onChange={handleChange}
                     />
                 </div>
 
