b8e326f4f84a17cd7e1d891e00df05bfbdb8d577
edilver <edilverpazu44@gmail.com>
2025-05-19 12:09:31 -0500

    corregido fondo negro de AddProject y AddTransaction

--- src/components/Modals/AddProjects.tsx
+++ src/components/Modals/AddProjects.tsx
@@ -40,7 +40,7 @@ export const NewProjectModal: React.FC<NewProjectModalProps> = ({
   };
 
   return (
-    <div className="fixed inset-0 bg-black flex items-center justify-center z-50 p-4">
+    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4">
       <div className="bg-white rounded-lg w-full max-w-md my-8">
         <div className="max-h-[90vh] overflow-y-auto">
           <div className="p-6">
